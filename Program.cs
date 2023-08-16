8f350f1cfa846ceb4e205ecb16b44c35536014b9
LS2021 <79998530+LS2021@users.noreply.github.com>
2023-08-16 22:05:45 +0200

    Finished Algorythm

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System.Numerics;
+using System.Text;
+using System.Threading.Tasks.Dataflow;
 
 string? seed;
 float Height;
@@ -15,17 +17,17 @@ Console.WriteLine(seed);
 do 
 {
     Console.Write("Pick Height (stl-units): ");
-} while (float.TryParse(Console.ReadLine(), out Height));
+} while (!float.TryParse(Console.ReadLine(), out Height));
 
 do
 {
     Console.Write("Pick a minimum diameter (stl-units): ");
-} while (float.TryParse(Console.ReadLine(), out MinDia));
+} while (!float.TryParse(Console.ReadLine(), out MinDia));
 
 do
 {
     Console.Write("Pick a Maximum diameter (stl-units): ");
-} while (float.TryParse(Console.ReadLine(), out MaxDia));
+} while (!float.TryParse(Console.ReadLine(), out MaxDia));
 
 DateTime now = DateTime.Now;
 
@@ -38,15 +40,15 @@ Vector3[] vertecies = new Vector3[vertCount];
 (uint A, uint B, uint C, Vector3 n)[] triangles = new (uint, uint, uint, Vector3 n)[triCount];
 
 vertecies[0] = Vector3.Zero;
-vertecies[vertCount] = new(0, 0, Height);
+vertecies[vertCount - 1] = new(0, 0, Height);
 
-uint vertId = 2;
+uint vertId = 1;
 
 for (uint i = 0; i < facets; i++)
 {
     float R = (rand.NextSingle() * (MaxDia  - MinDia) + MinDia) * 0.5f;
     float Phi = MathF.Tau / facets * (i + rand.NextSingle() - 0.5f);
-    vertecies[vertId] = new(R * MathF.Cos(Phi), R * Math.Sign(Phi), 0);
+    vertecies[vertId] = new(R * MathF.Cos(Phi), R * MathF.Sin(Phi), 0);
     vertId++;
 }
 
@@ -55,11 +57,11 @@ for (int j = 0; j < cuts; j++)
     for (uint i = 0; i < facets; i++)
     {
         float u = rand.NextSingle();
-        float Z = ((j == 0 ? -1f : -0.5f) * u + (j == (cuts - 1) ? 1f : 0.5f) * (1f - u)) * Height / (cuts + 1);
+        float Z = ((j == 0 ? -1f : -0.5f) * u + (j == (cuts - 1) ? 1f : 0.5f) * (1f - u) + j + 1) * Height / (cuts + 1);
 
         float R = (rand.NextSingle() * (MaxDia  - MinDia) + MinDia) * 0.5f;
         float Phi = MathF.Tau / facets * (i + rand.NextSingle() - 0.5f);
-        vertecies[vertId] = new(R * MathF.Cos(Phi), R * Math.Sign(Phi), Z);
+        vertecies[vertId] = new(R * MathF.Cos(Phi), R * MathF.Sin(Phi), Z);
         vertId++;
     }
 }
@@ -68,27 +70,90 @@ for (uint i = 0; i < facets; i++)
 {
     float R = (rand.NextSingle() * (MaxDia  - MinDia) + MinDia) * 0.5f;
     float Phi = MathF.Tau / facets * (i + rand.NextSingle() - 0.5f);
-    vertecies[vertId] = new(R * MathF.Cos(Phi), R * Math.Sign(Phi), Height);
+    vertecies[vertId] = new(R * MathF.Cos(Phi), R * MathF.Sin(Phi), Height);
     vertId++;
 }
 
 
 uint triId = 0;
 
-for (uint i = 0; i < facets; i++)
+for (uint i = 0; i < facets; i++) 
 {
-    
+    triangles[triId] = (0, (i + 1) % facets + 1, i + 1, new());
+    triId++;
 }
 
-for (int j = 0; j <= cuts; j++)
+for (uint j = 0; j <= cuts; j++)
 {
     for (uint i = 0; i < facets; i++)
     {
-
+        uint offset = facets * j + 1;
+        triangles[triId] = (offset + i, offset + (i + 1) % facets, offset + facets + i, new());
+        triId++;
+        triangles[triId] = (offset + (i + 1) % facets + facets, offset + facets + i, offset + (i + 1) % facets, new());
+        triId++;
     }
 }
 
 for (uint i = 0; i < facets; i++)
 {
-
+    triangles[triId] = (vertCount - 1, vertCount - facets - 1 + i, vertCount - facets - 1 + (i + 1) % facets, new());
+    triId++;
 }
+
+for(uint idx = 0; idx < triCount; idx ++)
+{
+    Vector3 A = vertecies[triangles[idx].A];
+    Vector3 B = vertecies[triangles[idx].B];
+    Vector3 C = vertecies[triangles[idx].C];
+
+    triangles[idx].n = Vector3.Normalize(Vector3.Cross(A - C, B - C));
+}
+
+
+Console.WriteLine("Pick File Save Location: ");
+
+string fileName = Console.ReadLine() ?? "";
+
+const int stlFileHeadderLength = 80;
+
+string headderText = $"seed: {seed}; Facets: {facets}; Cuts: {cuts}";
+byte[] hedderEncoded = Encoding.UTF8.GetBytes(headderText.PadRight(stlFileHeadderLength, '\0') ,0 ,stlFileHeadderLength); // stl-header must be exactly 80 bytes long;
+
+BinaryWriter OUT = new BinaryWriter(new FileStream(fileName, FileMode.Create));
+OUT.Write(hedderEncoded,0 , stlFileHeadderLength);
+
+OUT.Write(triCount);
+
+for(uint idx = 0; idx < triCount; idx ++)
+{
+    Vector3 A = vertecies[triangles[idx].A];
+    Vector3 B = vertecies[triangles[idx].B];
+    Vector3 C = vertecies[triangles[idx].C];
+    Vector3 n = triangles[idx].n;
+
+    ushort attribute = 0;
+
+    OUT.Write(n.X);
+    OUT.Write(n.Y);
+    OUT.Write(n.Z);
+
+    OUT.Write(A.X);
+    OUT.Write(A.Y);
+    OUT.Write(A.Z);
+
+    OUT.Write(B.X);
+    OUT.Write(B.Y);
+    OUT.Write(B.Z);
+
+    OUT.Write(C.X);
+    OUT.Write(C.Y);
+    OUT.Write(C.Z);
+
+    OUT.Write(attribute);
+}
+
+OUT.Flush();
+OUT.Close();
+
+Console.WriteLine("Done!");
