6bb12314a9d3591a7dddd4c2629c4d2d189cbb16
LS2021 <79998530+LS2021@users.noreply.github.com>
2023-08-17 19:04:38 +0200

    added option to set nuber of facets and cuts

--- Program.cs
+++ Program.cs
@@ -10,6 +10,8 @@ float MaxDia;
 uint cuts = 1;
 uint facets = 5;
 
+float angleRange = 0.8f;
+
 Console.Write("Pick A seed: ");
 seed = Console.ReadLine();
 Console.WriteLine(seed);
@@ -29,6 +31,16 @@ do
     Console.Write("Pick a Maximum diameter (stl-units): ");
 } while (!float.TryParse(Console.ReadLine(), out MaxDia));
 
+do
+{
+    Console.Write("Pick a number of facets (3 or more): ");
+} while (!uint.TryParse(Console.ReadLine(), out facets) && facets >= 3);
+
+do
+{
+    Console.Write("Pick a number of cuts (0 or more): ");
+} while (!uint.TryParse(Console.ReadLine(), out cuts) && cuts >= 0);
+
 DateTime now = DateTime.Now;
 
 Random rand = new(seed?.GetHashCode() ?? now.ToString().GetHashCode());
@@ -47,7 +59,7 @@ uint vertId = 1;
 for (uint i = 0; i < facets; i++)
 {
     float R = (rand.NextSingle() * (MaxDia  - MinDia) + MinDia) * 0.5f;
-    float Phi = MathF.Tau / facets * (i + rand.NextSingle() - 0.5f);
+    float Phi = MathF.Tau / facets * (i + rand.NextSingle() - 0.5f) * angleRange;
     vertecies[vertId] = new(R * MathF.Cos(Phi), R * MathF.Sin(Phi), 0);
     vertId++;
 }
@@ -60,7 +72,7 @@ for (int j = 0; j < cuts; j++)
         float Z = ((j == 0 ? -1f : -0.5f) * u + (j == (cuts - 1) ? 1f : 0.5f) * (1f - u) + j + 1) * Height / (cuts + 1);
 
         float R = (rand.NextSingle() * (MaxDia  - MinDia) + MinDia) * 0.5f;
-        float Phi = MathF.Tau / facets * (i + rand.NextSingle() - 0.5f);
+        float Phi = MathF.Tau / facets * (i + rand.NextSingle() - 0.5f) * angleRange;
         vertecies[vertId] = new(R * MathF.Cos(Phi), R * MathF.Sin(Phi), Z);
         vertId++;
     }
@@ -69,7 +81,7 @@ for (int j = 0; j < cuts; j++)
 for (uint i = 0; i < facets; i++)
 {
     float R = (rand.NextSingle() * (MaxDia  - MinDia) + MinDia) * 0.5f;
-    float Phi = MathF.Tau / facets * (i + rand.NextSingle() - 0.5f);
+    float Phi = MathF.Tau / facets * (i + rand.NextSingle() - 0.5f) * angleRange;
     vertecies[vertId] = new(R * MathF.Cos(Phi), R * MathF.Sin(Phi), Height);
     vertId++;
 }
